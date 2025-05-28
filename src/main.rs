5944b82be9602e88611c85d4f64d4e68ad147a83
edwar4rd <edwar4rd@gmail.com>
2025-05-28 15:10:17 +0800

    chap: 14.1 A Final Render

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,19 @@ use std::{io::stdout, rc::Rc};
 fn main() {
     env_logger::init();
     let image_settings = ImageSettings {
-        image_width: 400,
+        image_width: 1200,
         aspect_ratio: 16.0 / 9.0,
     };
     let quality_settings = QualitySettings {
-        samples_per_pixel: 100,
+        samples_per_pixel: 500,
         max_depth: 50,
     };
     let camera_settings = CameraSettings {
         vfov: 20.0,
-        focus_dist: 3.4,
-        defocus_angle: 10.0,
-        camera_center: Point3::new(-2.0, 2.0, 1.0),
-        camera_lookat: Point3::new(0.0, 0.0, -1.0),
+        focus_dist: 10.0,
+        defocus_angle: 0.6,
+        camera_center: Point3::new(13.0, 2.0, 3.0),
+        camera_lookat: Point3::new(0.0, 0.0, 0.0),
         camera_vup: Point3::new(0.0, 1.0, 0.0),
     };
 
@@ -35,40 +35,64 @@ fn main() {
 fn create_world() -> impl Hittable {
     let mut world = HittableList::new();
 
-    let material_ground = Lambertian::new(Point3::new(0.8, 0.8, 0.0));
-    let material_center = Lambertian::new(Point3::new(0.1, 0.2, 0.5));
-    let material_left = Dielectric::new(1.5);
-    let material_bubble = Dielectric::new(1.0 / 1.5);
-    let material_right = Metal::new(Point3::new(0.8, 0.6, 0.2), 1.0);
+    let ground_material = Lambertian::new(Point3::new(0.5, 0.5, 0.5));
+    world.push(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        Rc::new(ground_material),
+    ));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_material = rand::random::<f64>();
+            let center = Point3::new(
+                a as f64 + 0.9 * rand::random::<f64>(),
+                0.2,
+                b as f64 + 0.9 * rand::random::<f64>(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).magnitude() > 0.9 {
+                if choose_material < 0.8 {
+                    // diffuse
+                    let albedo = Point3::new(
+                        rand::random::<f64>() * rand::random::<f64>(),
+                        rand::random::<f64>() * rand::random::<f64>(),
+                        rand::random::<f64>() * rand::random::<f64>(),
+                    );
+                    world.push(Sphere::new(center, 0.2, Rc::new(Lambertian::new(albedo))));
+                } else if choose_material < 0.95 {
+                    // metal
+                    let albedo = Point3::new(
+                        0.5 * (1.0 + rand::random::<f64>()),
+                        0.5 * (1.0 + rand::random::<f64>()),
+                        0.5 * (1.0 + rand::random::<f64>()),
+                    );
+                    let fuzz = 0.5 * rand::random::<f64>();
+                    world.push(Sphere::new(center, 0.2, Rc::new(Metal::new(albedo, fuzz))));
+                } else {
+                    // glass
+                    world.push(Sphere::new(center, 0.2, Rc::new(Dielectric::new(1.5))));
+                }
+            }
+        }
+    }
 
     world.push(Sphere::new(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        Rc::new(material_ground),
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Dielectric::new(1.5)),
     ));
 
     world.push(Sphere::new(
-        Point3::new(0.0, 0.0, -1.2),
-        0.5,
-        Rc::new(material_center),
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Lambertian::new(Point3::new(0.4, 0.2, 0.1))),
     ));
 
     world.push(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        Rc::new(material_left),
-    ));
-
-    world.push(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        Rc::new(material_bubble),
-    ));
-
-    world.push(Sphere::new(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        Rc::new(material_right),
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Metal::new(Point3::new(0.7, 0.6, 0.5), 0.0)),
     ));
 
     world
